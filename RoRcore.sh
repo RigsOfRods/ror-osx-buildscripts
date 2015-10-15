c4e0b3016e25a2f8833d8ba2884ed5b974e8654e
Niklas Kersten <nkersten.dev@gmail.com>
2015-10-15 11:12:31 +0200

    updated for Ogre 1.9
    it's finally possible to start compiling RoR but there are compiler errors

--- RoRcore.sh
+++ RoRcore.sh
@@ -7,16 +7,15 @@ if [ ! -e rigs-of-rods ]; then
   git clone https://github.com/RigsOfRods/rigs-of-rods.git
 fi
 cd rigs-of-rods
-git checkout Ogre-2.0
 git pull
 
 cmake \
 -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
--DROR_USE_MYGUI="TRUE" \
--DROR_USE_OPENAL="TRUE" \
--DROR_USE_SOCKETW="TRUE" \
+-DOis_INCLUDE_DIRS=$ROR_INSTALL_DIR/include/OIS \
+-DOis_LIBRARIES=$ROR_INSTALL_DIR/lib/libOIS.a \
+-DROR_BUILD_CONFIGURATOR=OFF \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
--DCMAKE_CXX_FLAGS="-pipe -march=native" \
+-DCMAKE_CXX_FLAGS="-pipe -march=native -stdlib=libc++ -std=c++11 -DNOLANG" \
 .
 
 # CMAKE_CXX_FLAGS (flags for compiler) - Default are:
@@ -28,7 +27,7 @@ cmake \
 
 
 make $ROR_MAKEOPTS
-sed -i '/^PluginFolder=/d' bin/plugins.cfg
+sed -i .orig '/^PluginFolder=/d' bin/plugins.cfg
 echo "PluginFolder=$ROR_INSTALL_DIR/lib/OGRE" >>bin/plugins.cfg
 
 # there's no make install target, so just copy the bin folder
--- RoRdeps.sh
+++ RoRdeps.sh
@@ -44,15 +44,16 @@ make install
 # OGRE
 cd "$ROR_SOURCE_DIR"
 if [ ! -e ogre ]; then
-  hg clone https://bitbucket.org/sinbad/ogre -b v2-0
+  hg clone https://bitbucket.org/sinbad/ogre -b v1-9
 fi
 cd ogre
-hg pull && hg update
+hg pull -r 1-9 && hg update
 cmake \
 -DCMAKE_CXX_FLAGS="-stdlib=libc++ -std=c++11" \
 -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
 -DFREETYPE_INCLUDE_DIR=/usr/include/freetype2/ \
 -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+-DOGRE_BUILD_RENDERSYSTEM_GL3PLUS=ON \
 -DOGRE_BUILD_SAMPLES:BOOL=OFF .
 make $ROR_MAKEOPTS
 make install
@@ -63,7 +64,6 @@ if [ ! -e mygui ]; then
   git clone https://github.com/MyGUI/mygui.git
 fi
 cd mygui
-git checkout ogre2
 git pull
 cmake \
 -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
@@ -78,6 +78,35 @@ cmake \
 make $ROR_MAKEOPTS
 make install
 
+# Paged Geometry
+cd "$ROR_SOURCE_DIR"
+if [ ! -e ogre-paged ]; then
+  git clone https://github.com/Hiradur/ogre-paged.git
+fi
+cd ogre-paged
+git pull
+cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" \
+-DPAGEDGEOMETRY_BUILD_SAMPLES:BOOL=OFF \
+-DCMAKE_CXX_FLAGS="-stdlib=libc++ -std=c++11" \
+-DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo .
+make $ROR_MAKEOPTS
+make install
+
+# Caelum
+cd "$ROR_SOURCE_DIR"
+if [ ! -e caelum ]; then
+  git clone https://github.com/RigsOfRods/caelum
+fi
+cd caelum
+git pull
+cmake -DCMAKE_INSTALL_PREFIX="$ROR_INSTALL_DIR" 
+-DCMAKE_CXX_FLAGS="-stdlib=libc++ -std=c++11" \
+.
+make $ROR_MAKEOPTS
+make install
+# important step, so the plugin can load:
+ln -sf "$ROR_INSTALL_DIR/lib/libCaelum.so" "$ROR_INSTALL_DIR/lib/OGRE/"
+
 # MySocketW
 cd "$ROR_SOURCE_DIR"
 if [ ! -e mysocketw ]; then
@@ -88,3 +117,19 @@ git pull
 sed -i .orig '/^PREFIX *=/d' Makefile.conf
 make CXXFLAGS="-stdlib=libc++ -std=c++11" $ROR_MAKEOPTS dylib
 PREFIX="$ROR_INSTALL_DIR" make installosx
+
+# Angelscript
+cd "$ROR_SOURCE_DIR"
+if [ ! -e angelscript ]; then
+  mkdir angelscript
+fi
+cd angelscript
+wget -c http://www.angelcode.com/angelscript/sdk/files/angelscript_2.22.1.zip
+unzip -o angelscript_*.zip
+cd "sdk/angelscript/projects/gnuc macosx"
+sed -i .orig '/^LOCAL *=/d' makefile
+# make fails when making the symbolic link, this removes the existing versions
+rm -f ../../lib/*
+SHARED=1 VERSION=2.22.1 make CFLAGS="-stdlib=libc++ -std=c++11" CXXFLAGS="-stdlib=libc++ -std=c++11" $ROR_MAKEOPTS
+rm -f ../../lib/*
+SHARED=1 VERSION=2.22.1 LOCAL="$ROR_INSTALL_DIR" make -s install
